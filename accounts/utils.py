5dc36a9ffa64191a75717a943af90ffaa551bc2d
mohaahmodu <mohammedahmodu167@gmail.com>
2026-08-04 10:54:04 +0100

    Temporarily disable OTP email for debugging

--- accounts/utils.py
+++ accounts/utils.py
@@ -8,22 +8,4 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def send_otp_email(user, otp):
-    send_mail(
-        subject="Verify Your NACOS ExamBite Account",
-        message=f"""
-Hello {user.username},
-
-Your verification code is:
-
-{otp}
-
-This code expires in 10 minutes.
-
-If you didn't create an account, ignore this email.
-
-NACOS ExamBite Team
-""",
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-        fail_silently=False,
-    )
+    print(f"OTP for {user.email}: {otp}")
--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "exambite.onrender.com",
-    "localhost",
-    "127.0.0.1",
 ]
 
 
@@ -144,6 +142,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 30
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
