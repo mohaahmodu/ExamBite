44118833040c47c7c2770597dfd81664b8f050e9
mohaahmodu <mohammedahmodu167@gmail.com>
2026-08-05 23:10:16 +0100

    Remove automatic admin creation

--- dashboard/views.py
+++ dashboard/views.py
@@ -50,11 +50,3 @@ def dashboard(request):
         context
     )
 
-from django.contrib.auth.models import User
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="Admin12345"
-    )
